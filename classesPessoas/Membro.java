f84b81866c60ffc6857b254836f149ad308485f2
Maísa Pinheiro <141690423+Maisa-Pinheiro@users.noreply.github.com>
2023-08-26 15:40:30 -0300

    classe de membros

--- classesPessoas/Membro.java
+++ classesPessoas/Membro.java
@@ -1,37 +1,42 @@
-/*pacote que contem apenas as classes de pessoas: usuário, funcionario e visitante*/
-/*teste fernadna*/
+/*pacote que contem apenas as classes de pessoas: aluno, porfessor e funcionario */
+
 package classesPessoas;
 
-import classesObras.Livro;
+import classesObras.Multimidia;
 
 /*
- * classe dedicada apenas aos usuarios, os atributos publicos são os que mudam
+ * classe dedicada apenas aos membros, considerando que tanto alunos e professores como funcionários podem
+ * emprestar livrod, os atributos publicos são os que mudam
  * com frequência e podem ser acessados por outras classes sem necessidade de
  * getters e setters
  */
-public class Usuario {
+public class Membros {
 	private String nome;
 	private int cpf;
 	private int registro;
 	public int emprestimo; // quantos livros o usuario tem emprestado
-	public Livro[] livros_empres;// vetor que guarda os livros emprestados pelo usuário
+	private String categoria; // aluno grad, aluno pos, funcionário(especificar cargo) ou professor
+	public Multimidia[] itens_empres;// vetor que guarda os itens emprestados pelo usuário
 	public String prazo; // prazo de devolucao
 	public int atraso; // o numero de atraso em dias
 	public int idade; // idade do usuario
-	private String endereco;
+	private String endereco; 
+	private int salario;// para funcionário da biblioteca, aluno e professor, "não se aplica"
 
 	/* construtor dedicado apenas aos usuarios */
-	public Usuario(String nome, int cpf, int registro, int emprestimo, String prazo, int atraso, int idade,
-			String endereco) {
+	public Usuario(String nome, int cpf, int registro, int emprestimo, String categoria, String prazo, int atraso, int idade,
+			String endereco, int salario) {
 		this.nome = nome;
 		this.cpf = cpf;
 		this.registro = registro;
 		this.emprestimo = emprestimo;
+		this.categoria = categoria;
 		this.prazo = prazo;
 		this.atraso = atraso;
 		this.idade = idade;
 		this.endereco = endereco;
 		this.livros_empres = new Livro[5];
+		this.salario =salario;
 	}
 
 	/* getters para os atributos privados */
@@ -50,6 +55,12 @@ public class Usuario {
 	public String getendereco() {
 		return endereco;
 	}
+	public String getcategoria() {
+		return categoria;
+	}
+	public String getsalario() {
+		return salario;
+	}
 
 	/*
 	 * setters dos atributos que não mudam com frequência(atributos que não serão
@@ -66,15 +77,18 @@ public class Usuario {
 	public void setendereco(String endereco) {
 		this.endereco = endereco;
 	}
+	public void setsalario(String salario) {
+		this.salario = salario;
+	}
 
 	/*
 	 * implementação de um metodo para emprestar livros a um usuario(pode ser
 	 * considerado um setter?)
 	 */
-	public void emprestarLivro(Livro livro) {
+	public void emprestarItem(Multimidia livro) {
 		for (int i = 0; i < 5; i++) {
-			if (livros_empres[i] == null) {
-				livros_empres[i] = livro;
+			if (itens_empres[i] == null) {
+				itens_empres[i] = livro;
 				return;
 			}
 		}
@@ -82,22 +96,22 @@ public class Usuario {
 	}
 
 	/* método para mostrar quais livros a pessoa emprestou */
-	public void livrosEmprestados() {
-		System.out.println("Livros emprestados para o usuário " + nome + ":");
-		for (Livro livro : livros_empres) {
+	public void ItensEmprestados() {
+		System.out.println("Itens emprestados para o usuário " + nome + ":");
+		for (Multimidia livro : itens_empres) {
 			if (livro != null) {
 				System.out.println("- " + livro.gettitulo());
 			}
 		}
 	}
-	public void qtsLivros() {
+	public void qtsItens() {
 	    int a=0;
 		for (int i = 0; i < 5; i++) {
-			if (livros_empres[i] != null) {
+			if (itens_empres[i] != null) {
 				a= a + 1;
 			}
 			
 		}
-		System.out.println("O usuário " + nome + " emprestou " + a + " livros.");
+		System.out.println("O usuário " + nome + " emprestou " + a + " itens.");
 	}
 }
