c744f40ad300dad8f6bfcd3fa2978e2fdb3ad4ff
Maísa Pinheiro <141690423+Maisa-Pinheiro@users.noreply.github.com>
2023-08-27 13:45:02 -0300

    Update Relatorio.java

--- classesRelatorio/Relatorio.java
+++ classesRelatorio/Relatorio.java
@@ -16,12 +16,12 @@ import classesEmprestimo.Emprestimo;
  
 public class Relatorio {
 	private Multimidia[] acervo; // todos os itens de emprestimo que a biblioteca possui
-	private Membros[] membros; // todos os membros da biblioteca que podem fazer empréstimos
+	private Membro[] membros; // todos os membros da biblioteca que podem fazer empréstimos
 	private Funcionarios[] funcionarios; // todos os funcionarios da biblioteca
-	private Emprestimos[] emprestimo; // todos os emprestimos feitos pela biblioteca
+	private Emprestimo[] emprestimo; // todos os emprestimos feitos pela biblioteca
 	private LocalDate data; //data de emissão do relatório
 	private int numero; // numero do relatório
-	private RenovacaoReserva[] renovacoes;// todas as renovações e reservas feitas
+	private Renovacao[] renovacoes;// todas as renovações e reservas feitas
 	
 
 	/* construtor dedicado apenas aos usuarios */
@@ -32,7 +32,7 @@ public class Relatorio {
 	}
 
 	/* getters para os atributos privados */
-	public String getdata() {
+	public LocalDate getdata() {
 		return data;
 	}
 
@@ -40,9 +40,7 @@ public class Relatorio {
 		return numero;
 	}
 
-	/*
-	 * a classe não possui setters pois os atributos não podem ser modificados(seria outro relatótio)
-	 */
+	
 	 
 	 /*métodos para a emissão do relatótio*/
 	 
@@ -50,11 +48,11 @@ public class Relatorio {
 	 
 	 public void EmprestimosAtrasados() {
         int count = 0;
-        float totalMultas= 0.00;
+        float totalMultas= 0;
         int totalemprestimos = 0;
         LocalDate hoje = LocalDate.now();
 
-        for (Emprestimos emprestimo : emprestimo) {
+        for (Emprestimo emprestimo : emprestimo) {
             LocalDate devolucao = emprestimo.getdevolucao();
             if (devolucao.isBefore(hoje)) {
                 count++;
@@ -74,53 +72,50 @@ public class Relatorio {
     public void membros(){
         int qdtmembros=0;
         for( Membro membro : membros){
-            if(membros[i]!=null){
+            if(membro!=null){
                 qdtmembros++;
             }
         }
         System.out.println("Atualmente existem " + qdtmembros + " membros na biblioteca.");
     }
     public void funcionarios(){
-        int qdtfuncionarios=0;
+        int qtdfuncionarios=0;
         for( Funcionarios funcionario : funcionarios){
-            if(funcionarios[i]!=null){
-                qdtfuncionarios++;
+            if(funcionario!=null){
+                qtdfuncionarios++;
             }
         }
-        System.out.println("Atualmente existem " + qdtfuncionarios + " funcionarios na biblioteca.");
+        System.out.println("Atualmente existem " + qtdfuncionarios + " funcionarios na biblioteca.");
     }
     
     
     //total de itens para empréstimo
     public void qtdlivros(){
-        int qdtitens=0;
-        int qdtdisponível=0;
-        for( Multimidia acervo : acervo){
-            if(acervo[i]!=null){
-                qdtitens= qdtitens + acervo[i].copias;
-                qdtdisponivel = qdtdisponivel + acervo[i].disponivel;
+        int qtditens=0;
+        int qtddisponivel=0;
+        for( Multimidia acervos : acervo){
+            if(acervos!=null){
+                qtditens= qtditens + acervos.copias;
+                qtddisponivel = qtddisponivel + acervos.disponivel;
             }
         }
-        System.out.println("Atualmente existem " + qdtitens + " itens multimídia na biblioteca, dos quais " + qdtdisponivel + " estão disponíveis para empréstimo.");
+        System.out.println("Atualmente existem " + qtditens + " itens multimídia na biblioteca, dos quais " + qtddisponivel + " estão disponíveis para empréstimo.");
     }
     
     
     //total de renovações e reservas
     public void reservas(){
-        int qdtreservas=0;
-        int qdtrenovacoes=0;
-        for( RenovacaoReserva renovacao : renovacoes){
-            if(renovacoes[i].getreserva()==true){
-                qdtreservas++;
-            }else if(renovacoes[i].getreserva()==false){
-                qdtrenovacoes++;
+        int qtdreservas=0;
+        int qtdrenovacoes=0;
+        for( Renovacao renovacao : renovacoes){
+            if(renovacao.gettipo()==true){
+                qtdreservas++;
+            }else if(renovacao.gettipo()==false){
+                qtdrenovacoes++;
             }
         }
-        System.out.println("Até o presente dia foram feitas " + qdtreservas + " reservas e "+ qdtrenovacoes + " renovações na biblioteca.");
+        System.out.println("Até o presente dia foram feitas " + qtdreservas + " reservas e "+ qtdrenovacoes + " renovações na biblioteca.");
     }
     
-    
-
-	
-}
+ }
 
