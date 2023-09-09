dc32c702b61468f19cd963a08b01ea3bf90e572c
Maísa Pinheiro <141690423+Maisa-Pinheiro@users.noreply.github.com>
2023-09-09 17:10:53 -0300

    Add files via upload

---
+++ Sistema Biblioteca/src/biblioteca/models/GerenciadorEmprestimos.java
@@ -0,0 +1,18 @@
+package emprestimoPackage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciadorEmprestimos {
+
+    private static List<Emprestimo> todosEmprestimos = new ArrayList<>();
+
+    public static void adicionarEmprestimo(Emprestimo emprestimo) {
+        todosEmprestimos.add(emprestimo);
+    }
+
+    public static List<Emprestimo> obterTodosEmprestimos() {
+        return todosEmprestimos;
+    }
+    
+}
---
+++ Sistema Biblioteca/src/biblioteca/models/LivroFisico.java
@@ -0,0 +1,37 @@
+package multimidiaPackage;
+
+/*subclasse de Multimidia "LivroFisico"*/
+public class LivroFisico extends Multimidia {
+    private long isbn; // International Standard Book Number
+    private int edicao;
+    private String local; // localização do livro (estante e prateleira)
+    private String estadoConsevacao; // estado de conservação do livro físico
+
+    public LivroFisico(String titulo, int registro, String autor, String editora, int anoPublicacao,
+    String sinopse, String capa, String historicoEmprestimo, boolean disponibilidade, int numCopias,
+    int numCopiasDisponiveis, long isbn, int edicao, String local, String estadoConsevacao) {
+        super(titulo, registro, autor, editora, anoPublicacao, sinopse, capa, historicoEmprestimo, disponibilidade,
+                numCopias, numCopiasDisponiveis);
+        this.isbn = isbn;
+        this.edicao = edicao;
+        this.edicao = edicao;
+        this.local = local;
+        this.estadoConsevacao = estadoConsevacao;
+    }
+
+    public long getisbn() {
+        return isbn;
+    }
+
+    public int getedicao() {
+        return edicao;
+    }
+    
+    public String getlocal() {
+        return local;
+    }
+
+    public String getestadoConsevacao() {
+        return estadoConsevacao;
+    }
+}
