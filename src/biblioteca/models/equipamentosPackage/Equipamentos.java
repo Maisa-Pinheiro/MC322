eb519faded616e669bb104cdb4b3cced8bbd2dc1
FernandaSantos123 <f243385@dac.unicamp.br>
2023-09-10 13:19:27 -0300

    changes

---
+++ src/biblioteca/models/equipamentosPackage/Equipamentos.java
@@ -0,0 +1,141 @@
+package equipamentosPackage;
+
+public class Equipamentos {
+
+    private int id; // número de identificação interno para o recurso de multimidia
+    private int serialNumber; // número de serie do item
+    private String tipo; // qual é o tipo do item (ex.: Informatica, Audio Visual ou impressão)
+    private String marca; // marca do item
+    private String modelo; // modelo do item
+    private String estadoConservacao; // qual é o estado de conservação do item
+
+    /* construtor da classe externa */
+
+    public Equipamentos(int id, int serialNumber, String tipo, String marca, String modelo, String estadoConservacao) {
+        this.id = id;
+        this.serialNumber = serialNumber;
+        this.tipo = tipo;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.estadoConservacao = estadoConservacao;
+    }
+
+    /* getters da classe externa */
+
+    public int getid() {
+        return id;
+    }
+
+    public int getserialNumber() {
+        return serialNumber;
+    }
+
+    public String getmarca() {
+        return marca;
+    }
+
+    public String getmodelo() {
+        return modelo;
+    }
+
+    public String gettipo() {
+        return tipo;
+    }
+
+    public String getestadoConservacao() {
+        return estadoConservacao;
+    }
+
+    /* definição das classes internas */
+    public class Informatica {
+        private String acesso; // caso haja alguma restrição no acesso (por exemplo, necessidade de login)
+        private String detalhes; // mais detalhes sobre o item de acordo com a nessecidade (Ex.: processador,
+                                 // tela, sistema operacional e etc.)
+
+        /* construtor da classe interna */
+
+        public Informatica(String acesso, String detalhes) {
+            this.acesso = acesso;
+            this.detalhes = detalhes;
+        }
+
+        /* getters da classe interna */
+
+        public String getacesso() {
+            return acesso;
+        }
+
+        public String getdetalhes() {
+            return detalhes;
+        }
+    }
+
+    public class AudioVisual {
+        private String descricao; // descrição do item (ex.: câmeras, microfones, caixas de som, etc.)
+        private String kit; // se o item faz parte de um kit - quais são os item do kit
+        private String conexao; // qual tipo de conexão (bluetoth ou cabo)
+
+        /* construtor da classe interna */
+
+        public AudioVisual(String descricao, String kit, String conexao) {
+            this.descricao = descricao;
+            this.kit = kit;
+            this.conexao = conexao;
+        }
+
+        /* getters da classe interna */
+
+        public String getdescricao() {
+            return descricao;
+        }
+
+        public String getkit() {
+            return kit;
+        }
+
+        public String conexao() {
+            return conexao;
+        }
+    }
+
+    public class Impressao {
+        private String descricao; // descrição do item (ex.: Impressoras, scanners, copiadoras, etc.)
+        private String conexao; // qual tipo de conexão (bluetoth ou cabo)
+        private String manual; // futuramente pode se atrelar aqui um manual do dispositivo (como conectar-se a
+                               // ele, por exemplo)
+        private String detalhes; // detalhes adicionais do item
+
+        /* construtor da classe interna */
+
+        public Impressao(String descricao, String conexao, String manual, String detalhes) {
+            this.descricao = descricao;
+            this.conexao = conexao;
+            this.manual = manual;
+            this.detalhes = detalhes;
+        }
+
+        /* getters da classe interna */
+
+        public String getdescricao() {
+            return descricao;
+        }
+
+        public String getconexao() {
+            return conexao;
+        }
+
+        public String getmanual() {
+            return manual;
+        }
+
+        public String getdetalhes() {
+            return detalhes;
+        }
+    }
+
+    public enum CatogoriaEquipamento {
+        INFORMATICA,
+        AUDIOVISUAL,
+        IMPRESSÃO
+    }
+}
---
+++ src/biblioteca/models/eventosPackage/EventosBiblioteca.java
@@ -0,0 +1,125 @@
+package eventosPackage;
+
+import java.time.LocalTime;
+import java.time.LocalDate;
+
+public class EventosBiblioteca {
+    private int id; // número de identificação da palestra
+    public String local; // local da palestra
+    private String topico; // tema principal da palestra
+
+    public EventosBiblioteca(int id, String local, String topico){
+        this.id = id;
+        this.local = local;
+        this.topico = topico;
+    }
+
+    //getters dos atributos privados classe interna, eles não possuem setters pois, se mudassem, seria outro evento
+    public int getid(){
+        return id;
+    }
+    public String getlocal(){
+        return local;
+    }
+    public String gettopico(){
+        return topico;
+    }
+
+    /* definição das classes internas */
+    public class Palestra {//atributos públicos estão sujeitos à possíveis mudanças
+        
+        private String palestrante; // nome do palestrante
+        public LocalTime hora; // horario de inicio da palestra
+        private int duracao; // duração, em horas, da palestra
+        public LocalDate data; // data da palestra
+
+        /* construtor da classe interna */
+
+        public Palestra( String palestrante, LocalTime hora, int duracao, LocalDate data) {
+            this.palestrante = palestrante;
+            this.hora = hora;
+            this.duracao = duracao;
+            this.data = data;
+        }
+
+        /* getters dos atributos privados classe interna, eles não possuem setters pois, se mudassem, seria outra palestra */
+
+      
+
+        public String getpalestrante() {
+            return palestrante;
+        }
+
+        public int getduracao() {
+            return duracao;
+        }
+    }
+
+    public class Workshop {//atributos públicos estão sujeitos à possíveis mudanças
+    
+        private String instrutor; // nome do instrutor
+        public LocalTime hora; // horario de inicio do workshop
+        private int duracao; // duração, em horas, do workshop
+        public LocalDate data; // data do workshop
+        private String materiaisNecessarios; // materiais necessários para a realização do workshop
+
+        /* construtor da classe interna */
+
+        public Workshop( String instrutor, LocalTime hora, int duracao, LocalDate data, String materiaisNecessarios) {
+            this.instrutor = instrutor;
+            this.hora = hora;
+            this.duracao = duracao;
+            this.data = data;
+            this.materiaisNecessarios = materiaisNecessarios;
+        }
+
+        /* getters dos atributos privados classe interna, eles não possuem setters pois, se mudassem, seria outro workshop */
+
+        public String getinstrutor() {
+            return instrutor;
+        }
+        public int getduracao() {
+            return duracao;
+        }
+        public String getmateriais() {
+            return materiaisNecessarios;
+        }
+    }
+
+    public class Exposicao {//atributos públicos estão sujeitos à possíveis mudanças
+        private String expositor; // nome da empresa ou das pessoas responsáveis pela exposição
+        public LocalTime horainicio; // horario de inicio da exposição
+        public LocalTime horafim; // horario de encerramento da exposição
+        public int duracao; // duração, em dias, da exposição
+        public LocalDate data; // data da exposição
+        private String detalhes; // detalhes sobre o tema
+
+        /* construtor da classe interna */
+
+        public Exposicao( String expositor,  LocalTime horainicio , LocalTime horafim, int duracao, LocalDate data, String detalhes) {
+            this.expositor = expositor;
+            this.horainicio = horainicio;
+            this.horafim = horafim;
+            this.duracao = duracao;
+            this.data = data;
+            this.detalhes = detalhes;
+        }
+
+        /* getters dos atributos privados classe interna, eles não possuem setters pois, se mudassem, seria outro workshop */
+
+        public String getexpositor() {
+            return expositor;
+        }
+
+        public String getdetalhes() {
+            return detalhes;
+        }
+        
+    }
+
+    public enum TipoDeEvento {
+        PALESTERA,
+        WORKSHOP,
+        EXPOSICAO
+    }
+}
