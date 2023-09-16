846bde05a8d9f40a1fbbe767b8aa395c52222e2e
Maísa Pinheiro <141690423+Maisa-Pinheiro@users.noreply.github.com>
2023-09-16 14:32:57 -0300

    Update Renovacao.java

--- src/biblioteca/models/renovacaoReservaPackage/Renovacao.java
+++ src/biblioteca/models/renovacaoReservaPackage/Renovacao.java
@@ -13,19 +13,20 @@ import biblioteca.models.multimidiaPackage.Multimidia;
 
 public class Renovacao { // classe dedicada a renovações e reservas inteligentes
     private boolean tipo; // true - renovação, false- reserva
+    private static int proximoregistro = 1; // usado para fazer a atualização automática de registro
     private int registro; // registro de identificação renovação ou reserva
     private Pessoa pessoa; // pessoa que está fazendo a renovação ou reserva
 
     /* construtor dedicado apenas às usuarios */
-    private Renovacao(boolean tipo, int registro, Pessoa pessoa) {
+    private Renovacao(boolean tipo, Pessoa pessoa) {
         this.pessoa = pessoa;
         this.tipo = tipo;
-        this.registro = registro;
+        this.registro = proximoregistro++;
     }
 
     /* getters para os atributos privados */
     public String getpessoa() {
-        return pessoa.getnome();
+        return pessoa;
     }
 
     public boolean gettipo() {
@@ -89,18 +90,13 @@ public class Renovacao { // classe dedicada a renovações e reservas inteligent
     // reserva
     public void reservar(Multimidia item) {
         int copias = Multimidia.getnumCopiasDisponiveis();
-        if (item.disponibilidade == true) {
-            System.out.println("A cópia desejada do livro está disponivel para empréstimo, não é permitido reservá-la");
-        } else {
             if (copias > 0) {
                 System.out.println(
                         "Outras cópias do livro estão disponiveis para empréstimo, não é permitido reservar uma cópia em específico");
             } else {
-                copias = copias - 1;
                 System.out.println("O livro está reservado para " + pessoa.getnome()
                         + ", o usuário será avisado quando o mesmo puder ser retirado");
-            }
-        }
+            } 
     }
 
     public static Renovacao criarRenovacaoAprovacao(boolean tipo, int registro, Pessoa pessoa,
