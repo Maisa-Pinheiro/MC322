0d30dfca8f67f97022be6307ad880542efef2f9d
Maísa Pinheiro <141690423+Maisa-Pinheiro@users.noreply.github.com>
2023-09-30 18:33:59 -0300

    Update LivroFisico.java

--- src/biblioteca/models/multimidiaPackage/LivroFisico.java
+++ src/biblioteca/models/multimidiaPackage/LivroFisico.java
@@ -1,19 +1,32 @@
 package biblioteca.models.multimidiaPackage;
 
-/*subclasse de Multimidia "LivroFisico"*/
+import java.util.HashSet;
+import java.util.Set;
+
 public class LivroFisico extends Multimidia {
     private long isbn; // International Standard Book Number
     private int edicao;
     private String local; // localização do livro (estante e prateleira)
     private String estadoConsevacao; // estado de conservação do livro físico
 
+    // Mantenha um conjunto de ISBNs já utilizados
+    private static Set<Long> isbnsUtilizados = new HashSet<>();
+
     public LivroFisico(String titulo,  String autor, String editora, int anoPublicacao,
-    String sinopse, String capa,  boolean disponibilidade, int numCopias,
-    int numCopiasDisponiveis, long isbn, int edicao, String local, String estadoConsevacao, Categoria categoria) {
+            String sinopse, String capa, boolean disponibilidade, int numCopias,
+            int numCopiasDisponiveis, long isbn, int edicao, String local, String estadoConsevacao, Categoria categoria) {
         super(titulo,  autor, editora, anoPublicacao, sinopse, capa, disponibilidade,
                 numCopias, numCopiasDisponiveis, categoria);
+
+        // Verifique se o ISBN já está em uso
+        if (isbnsUtilizados.contains(isbn)) {
+            throw new IllegalArgumentException("ISBN já existente.");
+        }
+
+        // Adicione o ISBN ao conjunto de ISBNs utilizados
+        isbnsUtilizados.add(isbn);
+
         this.isbn = isbn;
-        this.edicao = edicao;
         this.edicao = edicao;
         this.local = local;
         this.estadoConsevacao = estadoConsevacao;
