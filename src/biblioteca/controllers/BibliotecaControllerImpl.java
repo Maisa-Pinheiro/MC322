5f883ff1c1243e2cc9d1956f80553757b2b8fdb3
Maísa Pinheiro <141690423+Maisa-Pinheiro@users.noreply.github.com>
2023-09-30 18:32:00 -0300

    Update BibliotecaControllerImpl.java

--- src/biblioteca/controllers/BibliotecaControllerImpl.java
+++ src/biblioteca/controllers/BibliotecaControllerImpl.java
@@ -8,27 +8,19 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 import biblioteca.models.emprestimoPackage.Emprestimo;
-import biblioteca.models.emprestimoPackage.ListaEmprestimos;
 import biblioteca.models.multimidiaPackage.Multimidia;
+import biblioteca.models.emprestimoPackage.Emprestimo;
 import biblioteca.models.renovacaoReservaPackage.Renovacao;
-
 import biblioteca.models.manutencaoPackage.Manutencao;
-//import biblioteca.models.multimidiaPackage.DVD_Video;
-//import biblioteca.models.multimidiaPackage.LivroEletronico;
-//import biblioteca.models.multimidiaPackage.LivroFisico;
-//import biblioteca.models.multimidiaPackage.Outros;
-
-import biblioteca.models.reservaSalaPackage.ReservaSala;
 import biblioteca.models.pessoasPackage.Pessoa;
-import biblioteca.models.renovacaoReservaPackage.ListaReserva;
-import biblioteca.models.equipamentosPackage.Equipamentos;
+
 
 public class BibliotecaControllerImpl implements BibliotecaController {
 
     private List<Multimidia> itens;
     private List<Emprestimo> emprestimos;
-    private Set<Multimidia.Categoria> categoriasusadas;
-    private List<Manutencao> itensmanutencao;           /*
+    private List<Manutencao> itensmanutencao;
+    private Set<Multimidia.Categoria> categoriasusadas; /*
                                                          * categorias de itens presentes na biblioteca, o enum categoria
                                                          * possui todas as categorias possíveis, mas este atributo reúne
                                                          * as categorias de livros que estão na biblioteca
@@ -51,64 +43,67 @@ public class BibliotecaControllerImpl implements BibliotecaController {
     }
 
     @Override
-    public List<Multimidia> consultarItensDisponiveis() {
+    public List<Multimidia> getitensDisponiveis(){
         return itens;
     }
 
     @Override
-    public void addemprestimo(Emprestimo emprestimo) {
-        emprestimos.add(emprestimo);
-    }
-
-    @Override
-
     public void addmanutencao(Manutencao manutencao){
         itensmanutencao.add(manutencao);
     }
 
     @Override
+    public List<Multimidia> consultarItensDisponiveis() {
+        return itens;
+    }
+
+    @Override
+    public void addemprestimo(Emprestimo emprestimo){
+        emprestimos.add(emprestimo);
+    }
+
+    @Override
     public void addItemDisponivel(Multimidia item){
-   
         itens.add(item);
     }
 
     @Override
-    public void removerItemDispoinvel(int id) {
+    public void removerItemDispoinvel(int id){
         for (Multimidia item : itens) {
-            if (id == item.getid()) {
-                item = null;
+            if(id == item.getid()){
+                item=null;
                 itens.remove(item);
             }
         }
     }
 
-    @Override
-    public void removeremprestimo(int id) {
+     @Override
+    public void removeremprestimo(int id){
         for (Emprestimo emprestimo : emprestimos) {
-            if (id == emprestimo.getregistro()) {
-                emprestimo = null;
+            if(id == emprestimo.getregistro()){
+                emprestimo=null;
                 emprestimos.remove(emprestimo);
             }
         }
     }
 
     @Override
-    public Multimidia retornaritem(int id) {
+    public Multimidia retornaritem(int id){
         for (Multimidia item : itens) {
-            if (id == item.getid()) {
-                return item;
-
+            if(id == item.getid()){
+               return item;
+               
             }
         }
         return null;
     }
 
     @Override
-    public Emprestimo retornaremprestimo(int id) {
+    public Emprestimo retornaremprestimo(int id){
         for (Emprestimo emprestimo : emprestimos) {
-            if (id == emprestimo.getregistro()) {
-                return emprestimo;
-
+            if(id == emprestimo.getregistro()){
+               return emprestimo;
+               
             }
         }
         return null;
@@ -117,8 +112,6 @@ public class BibliotecaControllerImpl implements BibliotecaController {
     @Override
     public void emprestarItem(Pessoa membro, Multimidia item)throws BloqueioMembroException {
        
-    public void emprestarItem(Pessoa membro, Multimidia item) {
-
         Boolean liberado = membro.getpodeemprestar();
         
     
@@ -130,13 +123,8 @@ public class BibliotecaControllerImpl implements BibliotecaController {
             item.numCopiasDisponiveis--;
             item.disponibilidade = false;
             membro.novoEmprestimo(emprestimo);
-            emprestimo.SetemprestimosSemRepeticao(emprestimo); // set para garantir que um item não seja emprestado para
-                                                               // dois membros ao mesmo tempo.
+            emprestimo.SetemprestimosSemRepeticao(emprestimo); // set para garantir que um item não seja emprestado para dois membros ao mesmo tempo.
             addemprestimo(emprestimo);
-            /* adicionando o emprestimo a classe generica */
-            ListaEmprestimos<Multimidia> listaDeEmprestimos = new ListaEmprestimos<>();
-            listaDeEmprestimos.addemprestimo(item);
-
             System.out.println("O item foi emprestado com sucesso.");
         } else {
              throw new IllegalArgumentException("o item não está disponível no momento");
@@ -154,47 +142,28 @@ public class BibliotecaControllerImpl implements BibliotecaController {
     }
 
     @Override
-    /* Reservar um item de multimidia */
-    public void reservaritem(Pessoa membro, Multimidia item) {
+    public void reservaritem(Pessoa membro, Multimidia item){
         if (item.disponibilidade == true) {
             System.out.println("o item está disponível para empréstimo");
-        } else {
+        }else{
             Renovacao reserva = new Renovacao(false, membro);
             reserva.reservar(item);
             item.addreserva(reserva);
-            /* classe generica */
-            ListaReserva<Multimidia> listaDeItens = new ListaReserva<>();
-            listaDeItens.addreserva(item);
             System.out.println("o item foi reservado com sucesso");
 
         }
+            
     }
 
-    @Override
-    /* Reservar um Equipamento - Classe generica */
-    public void reservarEquipamento(Equipamentos equipamento) {
-        /* precisa se implementada a logica que analisa a necessidade de emprestimo */
-        ListaReserva<Equipamentos> listaDeItens = new ListaReserva<>();
-        listaDeItens.addreserva(equipamento);
-    }
-
-    @Override
-    /* Reservar uma Sala - Classe generica */
-    public void reservarSala(ReservaSala sala) {
-        /* precisa se implementada a logica que analisa a necessidade de emprestimo */
-        ListaReserva<ReservaSala> listaDeItens = new ListaReserva<>();
-        listaDeItens.addreserva(sala);
-    }
-
-    public void renovaremprestimo(Pessoa membro, Emprestimo emprestimo) {
-
-        Renovacao renovacao = new Renovacao(true, membro);
-        renovacao.renovar(emprestimo);
+    public void renovaremprestimo(Pessoa membro,Emprestimo emprestimo){
+        
+            Renovacao renovacao = new Renovacao(true, membro);
+            renovacao.renovar(emprestimo);
 
     }
 
     @Override
-    public void devolverItem(Pessoa membro, Multimidia item, boolean dano) throws ItemDanificadoException {
+    public void devolverItem(Pessoa membro, Multimidia item, boolean dano) throws BloqueioMembroException {
     boolean emprestado = false;
 
     // Verifique se o item está na lista de empréstimos da pessoa
@@ -204,6 +173,15 @@ public class BibliotecaControllerImpl implements BibliotecaController {
 
             LocalDate prazo = emprestimo.getdataDevolucao();
             LocalDate today = LocalDate.now();
+
+            // Verifique se o item está danificado
+            if (item.isDanificado()) {
+                // Lança a exceção ItemDanificadoException
+                LocalDate prazo2= today.plusDays(7);
+                Manutencao manutencao= new Manutencao(item, today, prazo2,  "leve", "cola");
+                addmanutencao(manutencao);
+                throw new ItemDanificadoException("O item está danificado e deve ir para manutenção.");
+            }
 
             if (prazo.isBefore(today)) {
                 long atraso2 = ChronoUnit.DAYS.between(today, prazo);
@@ -224,19 +202,12 @@ public class BibliotecaControllerImpl implements BibliotecaController {
                     item.disponibilidade = true;
                     System.out.println("O item foi devolvido com sucesso ");
                 } else {
-
                     try {
                         if (dano) {
                             // Verifique se o item está danificado
                             if (item.isDanificado()) {
                                 // Lança a exceção ItemDanificadoException
-                                LocalDate hoje1 = LocalDate.now();
-                                LocalDate prazo2= hoje1.plusDays(7);
-                                Manutencao manutencao= new Manutencao(item, hoje1, prazo2,  "leve", "cola");
-                                addmanutencao(manutencao);
-                                throw new ItemDanificadoException("O item está danificado e deve ir para manutenção.");
-                                
-
+                                throw new ItemDanificadoException("O item está danificado e não pode ser emprestado.");
                             } else {
                                 emprestarItem(item.getreservas().get(0).getpessoa(), item);
                             }
@@ -245,18 +216,6 @@ public class BibliotecaControllerImpl implements BibliotecaController {
                         }
                     } catch (BloqueioMembroException e) {
                         System.out.println("Erro ao emprestar o item: " + e.getMessage());
-
-                    emprestimos.remove(emprestimo);
-                    membro.removeremprestimo(emprestimo);
-                    if (item.getsize() == 0) {
-                        item.numCopiasDisponiveis++;
-                        item.disponibilidade = true;
-
-                        System.out.println("O item foi devolvido com sucesso ");
-                    } else {
-                        emprestarItem(item.getreservas().get(0).getpessoa(), item);
-                        item.remove(0);
-
                     }
                     item.remove(0);
                     System.out.println("O item foi devolvido com sucesso e emprestado ao membro "
