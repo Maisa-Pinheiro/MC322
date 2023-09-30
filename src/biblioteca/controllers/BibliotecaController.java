285e2c1c64bd61dacdf4bbcc8cd76d2802c9751e
Maísa Pinheiro <141690423+Maisa-Pinheiro@users.noreply.github.com>
2023-09-30 09:49:43 -0300

    Update BibliotecaController.java

--- src/biblioteca/controllers/BibliotecaController.java
+++ src/biblioteca/controllers/BibliotecaController.java
@@ -10,6 +10,7 @@ import biblioteca.models.multimidiaPackage.Multimidia;
 //import biblioteca.models.multimidiaPackage.Outros;
 import biblioteca.models.emprestimoPackage.*;
 import biblioteca.models.pessoasPackage.Pessoa;
+import biblioteca.models.manutencaoPackage.Manutencao;
 //import biblioteca.models.pessoasPackage.Professor;
 //import biblioteca.models.pessoasPackage.AlunoGraduacao;
 //import biblioteca.models.pessoasPackage.AlunoPosGraduacao;
@@ -19,18 +20,20 @@ import biblioteca.models.pessoasPackage.Pessoa;
 public interface BibliotecaController {
     List<Multimidia> consultarItensDisponiveis();
 
-    void emprestarItem(Pessoa membro, Multimidia item);
+    void emprestarItem(Pessoa membro, Multimidia item)throws BloqueioMembroException;
+    //void emprestarItem(Pessoa membro, Multimidia item);
 
     void reservaritem(Pessoa membro, Multimidia item);
 
     void renovaremprestimo(Pessoa membro,Emprestimo emprestimo);
 
-    void devolverItem(Pessoa membro, Multimidia item);
+    void devolverItem(Pessoa membro, Multimidia item, boolean dano) throws ItemDanificadoException;
 
     void addItemDisponivel(Multimidia item);
 
     void addemprestimo(Emprestimo emprestimo);
 
+     void addmanutencao(Manutencao manutencao);
 
     void liberacao(Pessoa pessoa);
 
