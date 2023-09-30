941d1ba9f0224d566fd89420d84b9ed84ae80dd2
Maísa Pinheiro <141690423+Maisa-Pinheiro@users.noreply.github.com>
2023-09-30 09:52:15 -0300

    Create ItemNaoEmprestadoException.java

---
+++ src/biblioteca/controllers/ItemNaoEmprestadoException.java
@@ -0,0 +1,7 @@
+package biblioteca.controllers;
+public class ItemNaoEmprestadoException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public ItemNaoEmprestadoException(String message) {
+        super(message);
+    }
+}
