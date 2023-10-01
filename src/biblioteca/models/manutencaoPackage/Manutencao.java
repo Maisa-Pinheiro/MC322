5e930d0f1eb7f457e34866d9fe36638fdbc58c98
FernandaSantos123 <f243385@dac.unicamp.br>
2023-10-01 15:48:20 -0300

    lab08

--- src/biblioteca/models/manutencaoPackage/Manutencao.java
+++ src/biblioteca/models/manutencaoPackage/Manutencao.java
@@ -1,19 +1,19 @@
 package biblioteca.models.manutencaoPackage;
+
 import java.time.LocalDate;
 import biblioteca.models.multimidiaPackage.Multimidia;
-
-
 
 public class Manutencao {
     private int proxid = 1;
     private int id;
-    private Multimidia item; 
-    private LocalDate datainicio; 
+    private Multimidia item;
+    private LocalDate datainicio;
     private LocalDate previsaotermino;
     private String gravidade; // gravidade do dano
     private String matnecessario; // material mecessário para o reparo
 
-    public Manutencao(Multimidia item, LocalDate datainicio, LocalDate previsaotermino, String gravidade, String matnecessario){
+    public Manutencao(Multimidia item, LocalDate datainicio, LocalDate previsaotermino, String gravidade,
+            String matnecessario) {
         this.item = item;
         this.datainicio = datainicio;
         this.previsaotermino = previsaotermino;
@@ -22,41 +22,43 @@ public class Manutencao {
         this.id = proxid++;
     }
 
-    public int getid(){
+    public int getid() {
         return id;
     }
-    
-    public LocalDate getdatainicio(){
+
+    public Multimidia getitem() {
+        return item;
+    }
+
+    public LocalDate getdatainicio() {
         return datainicio;
     }
 
-    public LocalDate getprevisao(){
+    public LocalDate getprevisao() {
         return previsaotermino;
     }
 
-    public String getgravidade(){
+    public String getgravidade() {
         return gravidade;
     }
 
-     public String getmatnecessario(){
+    public String getmatnecessario() {
         return matnecessario;
     }
 
-    public void setdatainicio(LocalDate datainicio){
+    public void setdatainicio(LocalDate datainicio) {
         this.datainicio = datainicio;
     }
 
-    public void setprevisao(LocalDate previsaotermino){
+    public void setprevisao(LocalDate previsaotermino) {
         this.previsaotermino = previsaotermino;
     }
 
-    public void setgravidade(String gravidade){
+    public void setgravidade(String gravidade) {
         this.gravidade = gravidade;
     }
 
-     public void setmatnecessario(String matnecessario){
+    public void setmatnecessario(String matnecessario) {
         this.matnecessario = matnecessario;
     }
 }
-
-
